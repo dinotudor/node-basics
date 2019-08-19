b9a60b274c93bd543387752312693aa8952fb451
Dino Tudor <dinotudor@gmail.com>
2019-08-19 19:18:42 +0200

    Get params

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const express = require("express");
 const server = express();
 
 // Query params = ?user=dino
+// Route Params = users/id
 
 server.get("/users", (req, res) => {
   const name = req.query.name;
-  res.json({ message: `Hello ${name}` });
+  return res.json({ message: `Hello ${name}` });
+});
+
+server.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  return res.json({ message: `Buscando usuario ${id}` });
 });
 
 server.listen(3000);
