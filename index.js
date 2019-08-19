6af5d2e984773447b58493d9aa3583945e8da02e
Dino Tudor <dinotudor@gmail.com>
2019-08-19 23:36:40 +0200

    check if user is in array

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ server.use((req, res, next) => {
 
   console.timeEnd('Request');
 });
-
+//check user exists - local middleware
 function checkUserExist(req, res, next) {
   if (!req.body.name) {
     return res.status(400).json({ error: 'Username is required' });
@@ -28,16 +28,26 @@ function checkUserExist(req, res, next) {
   return next();
 }
 
-//return all users
+//check if user is in array - local middleware
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(400).json({ error: 'User does not exist' });
+  }
+  req.user = user;
+  return next();
+}
+
+//GET - list all users
 server.get('/users', (req, res) => {
   res.json(users);
 });
 
-//GET - get all users QUERY
-server.get('/users', (req, res) => {
-  const name = req.query.name;
-  return res.json({ message: `Hello ${name}` });
-});
+//GET - list all users QUERY
+//server.get('/users', (req, res) => {
+//  const name = req.query.name;
+//  return res.json({ message: `Hello ${name}` });
+//});
 
 //GET PARAMS
 
@@ -45,10 +55,11 @@ server.get('/users', (req, res) => {
   const id = req.params.id;
   return res.json({ message: `Buscando usuario ${id}` });
 }); */
-//GET 'users' get one user
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params;
-  return res.json(users[index]);
+
+//GET - list one user
+server.get('/users/:index', checkUserInArray, (req, res) => {
+  //  const { index } = req.params; (As the middleware 'checkUserInArray' is defined, all methods using the middleware have acces to the variable
+  return res.json(req.user);
 });
 
 //POST - create new user (push to array)
@@ -61,7 +72,7 @@ server.post('/users', checkUserExist, (req, res) => {
 });
 
 //PUT - edit one user
-server.put('/users/:index', checkUserExist, (req, res) => {
+server.put('/users/:index', checkUserInArray, checkUserExist, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -72,7 +83,7 @@ server.put('/users/:index', checkUserExist, (req, res) => {
 });
 
 //DELETE - delete one user
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
