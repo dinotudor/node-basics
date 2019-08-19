ae6f9346fe3232c4082f3fd0820e9d53e89f69ed
Dino Tudor <dinotudor@gmail.com>
2019-08-19 21:50:20 +0200

    Post create new user local

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json()); //express can recieve data in JSON format
+
 // Query params = ?user=dino
 // Route Params = users/id
 // Request Body = { "name": "john", "email": "john@doe.com" }
@@ -15,21 +17,31 @@ server.get("/users", (req, res) => {
   res.json(users);
 });
 
-//QUERY
+//GET - get all users QUERY
 server.get("/users", (req, res) => {
   const name = req.query.name;
   return res.json({ message: `Hello ${name}` });
 });
 
-// PARAMS
+//GET PARAMS
+
 /* server.get("/users/:id", (req, res) => {
   const id = req.params.id;
   return res.json({ message: `Buscando usuario ${id}` });
 }); */
-
+//GET 'users' get one user
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
   return res.json(users[index]);
+});
+
+//POST - create new user (push to array)
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
 });
 
 server.listen(3000);
