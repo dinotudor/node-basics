1d1a45d53444383ba299fe057f85f5483abc734b
Dino Tudor <dinotudor@gmail.com>
2019-08-19 19:57:28 +0200

    get by index param

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const server = express();
 
 // Query params = ?user=dino
 // Route Params = users/id
+// Request Body = { "name": "john", "email": "john@doe.com" }
 
+const users = ["John", "Olivia", "Dino"];
+
+//QUERY
 server.get("/users", (req, res) => {
   const name = req.query.name;
   return res.json({ message: `Hello ${name}` });
 });
 
-server.get("/users/:id", (req, res) => {
+// PARAMS
+/* server.get("/users/:id", (req, res) => {
   const id = req.params.id;
   return res.json({ message: `Buscando usuario ${id}` });
+}); */
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
 });
 
 server.listen(3000);
